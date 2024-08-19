f53bac5d686f6fd0abdbf934ebdbd3d7dd8da59d
noamtoys <noamtoys10k@gmail.com>
2024-08-19 17:11:47 +0300

    ad

--- src/test/java/com/example/demo123/entity/CalculatorTest.java
+++ src/test/java/com/example/demo123/entity/CalculatorTest.java
@@ -11,7 +11,7 @@ public class CalculatorTest {
    public void addTest()
    {
        Calculator calculator = new Calculator();
-       int res = calculator.add(22,24);
+       int res = calculator.add(22,4);
        assertEquals(26 , res);
    }
 
